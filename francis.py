708c0a4cfc3d5b8e62f1fdb1f035ce45f1203696
arcadus <arcadus@users.noreply.github.com>
2016-11-24 22:04:13 -1000

    initial bot commit

---
+++ francis.py
@@ -0,0 +1,23 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+@asyncio.coroutine
+def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+@asyncio.coroutine
+def on_message(message):
+    if message.content.startswith('!sleep'):
+        yield from asyncio.sleep(5)
+        yield from client.send_message(message.channel, 'Done sleeping')
+    elif message.content.startswith('!test'):
+        yield from client.send_message(message.channel, 'hi')
+
+client.run('token')
